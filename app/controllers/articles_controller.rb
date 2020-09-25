5dfad27e2abe8c4d4f49bec25b85cc6c8a676445
michael wasserman <>
2020-09-25 09:05:14 -0400

    - new articles can now be added - validation feedback for article creation is now presented to the user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,23 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
+    def index 
+        @articles = Article.all
+    end
+
+    def new 
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :descripton))
+        if @article.save
+            #redirect_to article_path(@article) or
+            flash[:notice] = "Article was created successfully"
+            redirect_to @article
+        else 
+            render 'new'
+        end
+    end
 end
