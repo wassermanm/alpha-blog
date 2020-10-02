5f1e7d66715feabe9f2a4ce7f60d8a335542591f
michael wasserman <>
2020-10-02 09:21:07 -0400

    added one to many relationship between users and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
+        @article.user = User.first
         if @article.save
             #redirect_to article_path(@article) or
             flash[:notice] = "Article was created successfully"
