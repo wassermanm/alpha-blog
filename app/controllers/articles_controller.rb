bd5784632cc3460325d2f8d920c102869dd57b47
michael wasserman <>
2020-10-06 09:46:22 -0400

    moved methods to application_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         if @article.save
             #redirect_to article_path(@article) or
             flash[:notice] = "Article was created successfully"
